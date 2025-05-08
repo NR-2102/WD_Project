68fc250e563eed74e95c81fcfdbb1bbe6b91c69f
kxbir07 <kabirsharma1357@gmail.com>
2025-05-08 13:28:18 +0530

    comments

--- ecom/cart/apps.py
+++ ecom/cart/apps.py
@@ -1,6 +1,9 @@
+# Import the AppConfig class from django.apps
 from django.apps import AppConfig
 
-
+# Define a configuration class for the 'cart' app
 class CartConfig(AppConfig):
+    # Set the default type of primary key field to BigAutoField
     default_auto_field = 'django.db.models.BigAutoField'
+    # Set the name of the app
     name = 'cart'
