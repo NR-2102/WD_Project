d54baa1325cd96072348c212e545c8478493b15a
JAGDEV008 <jagdevdass1008@gmail.com>
2025-05-13 02:24:35 +0530

    2025

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,22 +1,32 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import models # 	To use Django's model fields and features
+from django.contrib.auth.models import User # To use Django's built-in user accounts
+
+# CharField(max_length=200) = stores the 'short text string'..
+# TextField() = store the long 'strings' or 'characters'..
+# OneToOneField = single profile to single user..
+# ForeignKey = many profiles to single user..
+# ManyToManyField = many profiles to many users..
+# ImageField = Stores image files..
+# PositiveIntegerField = store only non-negative integers..
+# DateTimeField = store both date and time in one field..
+# EmailField =  store email addresses..
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    
 
-    def __str__(self):
+    def __str__(self): # __str__ = magic method
         return self.name
 
 class Products(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
     price_inr = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
+    # on_delete=models.CASCADE = Delete this object if its category is deleted
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True) # (auto_now_add=True) =  Automatically set the value when the object is created
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -24,15 +34,15 @@ class Products(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.TextField(blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True) # TextField store the long 'strings' or 'characters'.. 
+    phone_number = models.CharField(max_length=15, blank=True) # CharField stores the 'short text string'..
     profile_picture = models.ImageField(upload_to='profile_pics/', default='default.jpg')
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
 class Order(models.Model):
-    STATUS_CHOICES = (
+    STATUS_CHOICES = ( # (p, P) --> (p = store in database); (P = show on site)
         ('pending', 'Pending'),
         ('processing', 'Processing'),
         ('shipped', 'Shipped'),
